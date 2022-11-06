fce78cffb3a6cb6c36ba2d9ef2860e654d059638
Generalbang <ojodare73@gmail.com>
2022-11-06 17:11:51 +0100

    bug partially fixed

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-const myForm = document.querySelector('.myform')
-const first = document.querySelector('.first').value
-const last = document.querySelector('.last').value
-const email = document.querySelector('.email').value
-const pass = document.querySelector('.pass').value
+let myForm = document.querySelector('.myform')
+let first = document.querySelector('.first').value
+let last = document.querySelector('.last').value
+let email = document.querySelector('.email').value
+let pass = document.querySelector('.pass').value
 let submit = document.querySelector('.submit')
 
-const msg1 = document.querySelector('.msg1')
-const msg2 = document.querySelector('.msg2')
-const msg3 = document.querySelector('.msg3')
-const msg4 = document.querySelector('.msg4')
+let msg1 = document.querySelector('.msg1')
+let msg2 = document.querySelector('.msg2')
+let msg3 = document.querySelector('.msg3')
+let msg4 = document.querySelector('.msg4')
 let a = document.querySelectorAll('.a')
 
 submit.addEventListener('click', onSubmit);
 function onSubmit(e){
-    e.preventDefault();
+     e.preventDefault();
 
     
     if (first === ''){
-        msg1.textContent = 'First name can not be empty'
-        a.textContent = '<img src="./images/icon-error.svg" alt="">'
+        msg1.textContent = 'First name cannot be empty'
+        a.innerHTML = '<img src="./images/icon-error.svg" alt="">'
     }
+
     if (last === ''){
-        msg2.textContent = 'Last name can not be empty'
+        msg2.textContent = 'Last name cannot be empty'
         a.textContent = '<img src="./images/icon-error.svg" alt="">'
     }
+
     if (email == ',/<>!_+='){
-        msg3.textContent = 'Email not be empty'
+        msg3.textContent = 'Email cannot be empty'
         a.textContent = '<img src="./images/icon-error.svg" alt="">'
     }
+    
     if (pass === ''){
-        msg4.textContent = 'Password can not be empty'
+        // msg4.textContent = 'Password cannot be empty'
         a.textContent = '<img src="./images/icon-error.svg" alt="">'
+    }
+
+    else {
+        alert('Welcome to the club')
     }
 }
